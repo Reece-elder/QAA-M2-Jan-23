6de7a7477cfa3bee4ceeb86c0441d5aa3c0fc37f
reece-elder <reece.elder@qa.com>
2023-01-26 11:26:50 +0000

    basic python

--- code_examples/001-intro/demo.py
+++ code_examples/001-intro/demo.py
@@ -12,3 +12,41 @@ name = 123     # The type of a variable is dynamically read by the interpreter a
 
 # Outputs the content in the () to the terminal, so we can read it
 print(name)
+
+# Python is primarily a scripting language that 'typically' reads a file from top to bottom
+print(5)
+print(1)
+print(2)
+print("Hello world")
+
+# Python is dynamically typed meaning you don't need to explicitly specify type
+string = "Red"
+boolean = True # False
+int = 123
+float = 1.23
+
+# Arrays, collect data - tuples, lists, dictionaries, sets
+favFruits = ["Mango", "Kiwi", "Banana"] # List
+
+# print(boolean)
+# print(int)
+# print(float)
+# print(string) Ctrl + / 
+print(favFruits) # array.atObject <efef83e93038fef>
+
+# Naming conventions and best practice
+# variable names should only contain abcABC123 _ $
+# percentage% = 30 
+# last name = "no spaces allowed"
+# money$ = 1234
+# print = "a4 paper"
+GLOBAL_VARS = "ALL UPPERCASE AND _"
+camelCase = "my favourite"
+snake_case = "not my fav"
+PascalCase = "another type"
+
+# Inputs - Allow the user (us) to enter data into our python code
+# Python is typically a language designed for Devs to use to help with other tools 
+
+favColour = input("Please enter your fav colour: ")
+print("my fav colour is " + favColour) # String concatenation, adding multiple strings together
