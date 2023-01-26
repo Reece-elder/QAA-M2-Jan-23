c9eecf0adc366a41559586824a1b93477d2a4bdb
reece-elder <reece.elder@qa.com>
2023-01-26 10:36:42 +0000

    basic python code

---
+++ code_examples/001-intro/demo.py
@@ -0,0 +1,14 @@
+# Python is a dynamically typed scripting language
+# In ANY languages (that support comments), these lines are not read by an interpreter 
+# Used for information to other devs 
+
+# Dynamically typed - 
+# Variable is a placeholder of data
+
+# the variable called name is a String
+# String name = "reece"
+name = "reece" # At the minute this is a STRING 
+name = 123     # The type of a variable is dynamically read by the interpreter and can change
+
+# Outputs the content in the () to the terminal, so we can read it
+print(name)
