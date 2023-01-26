c6e199fa2b14dda3dc4bca38f4f815fbae00d610
reece-elder <reece.elder@qa.com>
2023-01-26 13:47:36 +0000

    more conditionals

--- code_examples/002-Conditionals/demo.py
+++ code_examples/002-Conditionals/demo.py
@@ -31,3 +31,30 @@ if colour.lower() == "red":
     print("colour is red")
 else:
     print("Colour is not red")
+
+cakeSize = 20
+frosting = False
+
+# Complex conditional statement with multiple conditions 
+# OR  - if condition 1 is true or condition 2 is true or both are true
+# AND - if cond 1 is true AND cond 2 is true
+
+if frosting == False and cakeSize > 21:
+    print("Frosting is false AND cake is bigger than 21")
+elif frosting == True:
+    print("Frosting is true")
+elif cakeSize > 21:
+    print("Cake is bigger than 21")
+else:
+    print("Cake is smaller than 21 and frosting is false")
+
+if frosting == False or cakeSize > 21:
+    print("either frosting is false, or cakesize is bigger than 21, or they are both true")
+
+    # When checking a boolean for True or False we can just put in the boolean we're checking
+    # Only works this way for True booleans
+    if frosting or colour == "red":
+        print("Cake size is bigger than 21 ")
+
+    if not frosting:
+        print("Cake is not frosted")
