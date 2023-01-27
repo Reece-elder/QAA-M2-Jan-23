4dc152d3786934f7b22ce9aa8bcd02f56f3725fc
reece-elder <reece.elder@qa.com>
2023-01-27 10:44:43 +0000

    factorial loops

--- code_examples/003-Iteration/lab.py
+++ code_examples/003-Iteration/lab.py
@@ -20,3 +20,35 @@ while current < goal:
 
 
 print(f"It took {years} years to increase {starting} to {goal} through interest of {interest * 100}%")
+
+
+# Guessing the number task 
+correct = False
+guesses = 0
+allowed_guesses = 4
+min = 10
+max = 40
+exact_num = 23
+
+# Loop 3 times (or however many guesses I allow)
+for x in range(allowed_guesses):
+
+    guess_num = int(input(f"Please enter a guess number between {min} and {max}: "))
+    if guess_num == exact_num:
+        print("Correct!")
+    else:
+        guesses += 1 # increase guesses by 1
+        # hot is within 4 nums, cold is within 15 nums
+        diff = guess_num - exact_num
+        print(abs(diff)) 
+
+
+# Factorial - Factorial 3 = 1 * 2 * 3 = 6
+
+total = 1
+factorial = 3
+
+for num in range(1, factorial + 1):
+    total = total * num
+
+print(total)
