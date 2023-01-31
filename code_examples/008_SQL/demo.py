3237d5cb6f98bd49b5080008f6881d0ea75cdfeb
reece-elder <reece.elder@qa.com>
2023-01-31 12:15:22 +0000

    complex functions

--- code_examples/008_SQL/demo.py
+++ code_examples/008_SQL/demo.py
@@ -4,6 +4,8 @@ import sqlite3 as sql
 # Creating a connection object, by running the connect() function
 conn = sql.connect("new-db") # If this file doesn't exist, create it. If it does, connect to it
 
+conn.execute("PRAGMA foreign_keys = 1") # forces Foreign Key restraints 
+
 # Making a cursor running the cursor() function that belongs to our conn object
 cursor = conn.cursor()
 
@@ -23,20 +25,43 @@ print(cursor.execute("SELECT name FROM sqlite_master WHERE type='table';").fetch
 # Data Manipulation Queries (Creating, Updating or Deleting Data)
 # Data Selection Queries (Getting data)
 
-def addData():
+def insertMovie():
+    movie_title = input("Please enter movie title: ")
     # query = "INSERT INTO <table name> (field_1, field_2, field_3) VALUES ('value_1', value_2, value_3);"
-    movie_query = "INSERT INTO movies (movie_title, genre, third_dimension_film) VALUES ('shrek 1', 'COM', true);"
-    ticket_query = "INSERT INTO tickets (film_id, number_tickets, ticket_name) VALUES (1, 4, 'Wesley Snipes');"
-
+    movie_query = f"INSERT INTO movies (movie_title, genre, third_dimension_film) VALUES ('{movie_title}', 'HOR', true);"
     cursor.execute(movie_query)
+    return True
+
+def insertTicket(film_id):
+    # query = "INSERT INTO <table name> (field_1, field_2, field_3) VALUES ('value_1', value_2, value_3);"
+    ticket_query = f"INSERT INTO tickets (film_id, number_tickets, ticket_name) VALUES ({film_id}, 5, 'Wesley Snipes');"
     cursor.execute(ticket_query)
+    return True
 
-    select_query = "SELECT * FROM movies" # Reading data from DBs, SELECT * what columns to see, 
-    # cursor selects the data, fetchAll() imports it to python
-    print(cursor.execute(select_query).fetchall())
-    print(cursor.execute("SELECT * FROM tickets").fetchall())
+def readAllData(table_name):
+    select_query = f"SELECT * FROM {table_name}"
+    return cursor.execute(select_query).fetchall()
 
-addData()
+def readAllMoviesGenre(genre):
+    query = f"SELECT * FROM movies WHERE genre='{genre}'"
+    return cursor.execute(query).fetchall()
+
+def readAllQuery(query):
+    return cursor.execute(query).fetchall()
+
+# insertMovie()
+# insertTicket(1)
+# insertTicket(2)
+# insertTicket(3)
+# insertTicket(12)
+# print(readAllMoviesGenre("HOR"))
+# print(readAllData("movies"))
+# print(readAllQuery("SELECT * FROM tickets WHERE number_tickets < 4"))
+# insertMovie()
+print(insertTicket(500))
+
+# Exercise - Implement a function that gets an animal by animal id (pass in id)
+# Create a function that gets all records from one condition the user can set (pass in field and value)
 
 # Saves your changes to the DB
 conn.commit()
