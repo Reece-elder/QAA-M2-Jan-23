84160db7155f55d281ad5f7c0b2f5c752b6c8454
reece-elder <reece.elder@qa.com>
2023-01-31 14:09:44 +0000

    basic cinema code

--- code_examples/008_SQL/cinema.sql
+++ code_examples/008_SQL/cinema.sql
@@ -12,3 +12,11 @@ CREATE TABLE tickets (
     ticket_name VARCHAR(40),
     FOREIGN KEY(film_id) REFERENCES movies(film_id)
 );
+
+INSERT INTO movies (movie_title, genre, third_dimension_film) VALUES ('Movie 1', 'HOR', true);
+INSERT INTO movies (movie_title, genre, third_dimension_film) VALUES ('Movie 2', 'COM', false);
+INSERT INTO movies (movie_title, genre, third_dimension_film) VALUES ('Movie 3', 'ACT', true);
+
+INSERT INTO tickets (film_id, number_tickets, ticket_name) VALUES (1, 4, 'Wesley Snipes');
+INSERT INTO tickets (film_id, number_tickets, ticket_name) VALUES (2, 3, 'Kate Beckinsdale');
+INSERT INTO tickets (film_id, number_tickets, ticket_name) VALUES (1, 6, 'Milla Jovovich');
